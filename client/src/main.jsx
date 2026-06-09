fe02f867c505a41874abd6a0288868048001b3b4
94tec <fixtone94ec@gmail.com>
2026-06-09 06:16:38 +0300

    fix: remove react-helmet-async in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,11 +5,8 @@ import './index.css';
 
 import App from './App.jsx';
 import ErrorBoundary from './components/ErrorBoundary';
-
 import { ShareProvider } from './context/ShareContext';
-
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { HelmetProvider } from 'react-helmet-async';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -20,14 +17,12 @@ const queryClient = new QueryClient({
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <QueryClientProvider client={queryClient}>
-        <ErrorBoundary>
-          <ShareProvider>
-            <App />
-          </ShareProvider>
-        </ErrorBoundary>
-      </QueryClientProvider>
-    </HelmetProvider>
+    <QueryClientProvider client={queryClient}>
+      <ErrorBoundary>
+        <ShareProvider>
+          <App />
+        </ShareProvider>
+      </ErrorBoundary>
+    </QueryClientProvider>
   </React.StrictMode>
 );
